128d490c838873f3a51af5f326224c9a82cbb41b
insal1998 <insalansari80@gmail.com>
2023-01-18 12:21:41 +0530

    Login API 18-01-23

--- application/controllers/api/Login.php
+++ application/controllers/api/Login.php
@@ -8,209 +8,56 @@ use Restserver\Libraries\REST_Controller;
 
 class Login extends REST_Controller
 {
-    public function index()
+    public function __construct()
     {
         parent::__construct();
         $this->load->database();
+        $this->load->model('api/Users_model');
+
+        // $this->load->model('api/registerpatient_model');
+
     }
 
-
-    public function login_get()
+    public function login_post()
     {
-        $data = $this->db->select('*')->from('patients')->get()->result();
-        if ($data) {
-            $this->response([
-                'status' => TRUE,
-                'data' => $data
-            ], REST_Controller::HTTP_OK);
-        } else {
-            $this->response([
-                "status" => False,
-                "Message" => "Internal Server Error"
-            ], REST_Controller::HTTP_BAD_REQUEST);
-        }
-    }
+        // Get the post data
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
 
+        // Validate the post data
+        if (!empty($email) && !empty($password)) {
 
-    public function loginnow()
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Check if any user exists with the given credentials
+            $con['returnType'] = 'single';
+            $con['conditions'] = array(
+                'email' => $email,
+                'password' => sha1($password),
+                'status' => 1
+            );
 
-            $this->form_validation->set_rules('email', 'Email', 'required');
-            $this->form_validation->set_rules('password', 'Password', 'required');
+            $user = $this->Users_model->getRows($con);
+            // $insertData = $this->registerpatient_model->getRows($);
+            if ($user) {
 
-            if ($this->form_validation->run() == TRUE) {
-
-                $email = $this->input->post('email');
-                $password = $this->input->post('password');
-                $this->load->model('user_model');
-
-                if ($this->input->post('login_type') == '') {
-
-                    $this->session->set_flashdata('error', 'Login Failed');
-                    redirect(base_url('welcome/login'));
-                }
-                //------------------------------------admin-login-----------------------------//
-                if ($this->input->post('login_type') == 'Admin') {
-
-                    $status = $this->user_model->checkPassword1($password, $email);
-                    if ($status != false) {
-
-                        $email = $status->email;
-
-                        $session_data = array(
-                            'email' => $email,
-                        );
-
-                        $this->session->set_userdata('AdminLoginSession', $session_data);
-                        redirect(base_url('welcome/doc_dash'));
-                    } else {
-                        $this->session->set_flashdata('error', 'Email or Password is Wrong');
-                        redirect(base_url('welcome/login'));
-                    }
-                }
-                //------------------------------------doctor-login-----------------------------//
-
-                elseif ($this->input->post('login_type') == 'doctor') {
-                    $status = $this->user_model->checkPassword2($password, $email);
-                    if ($status != false) {
-
-                        $email = $status->email;
-
-                        $session_data = array(
-                            'email' => $email,
-                        );
-
-                        $this->session->set_userdata('DoctorLoginSession', $session_data);
-                        redirect(base_url('welcome/doc_dash'));
-                    } else {
-                        $this->session->set_flashdata('error', 'Email or Password is Wrong');
-                        redirect(base_url('welcome/login'));
-                    }
-                }
-                //------------------------------------optometrists-login-----------------------------//
-
-                elseif ($this->input->post('login_type') == 'optometrists') {
-                    $status = $this->user_model->checkPassword5($password, $email);
-                    if ($status != false) {
-
-                        $email = $status->email;
-
-                        $session_data = array(
-                            'email' => $email,
-                        );
-
-                        $this->session->set_userdata('OptometristsLoginSession', $session_data);
-                        redirect(base_url('welcome/doc_das'));
-                    } else {
-                        $this->session->set_flashdata('error', 'Email or Password is Wrong');
-                        redirect(base_url('welcome/login'));
-                    }
-                }
-                //------------------------------------laboratorist-login-----------------------------//
-
-                elseif ($this->input->post('login_type') == 'laboratorist') {
-                    $status = $this->user_model->checkPassword3($password, $email);
-                    if ($status != false) {
-
-                        $email = $status->email;
-
-                        $session_data = array(
-                            'email' => $email,
-                        );
-
-                        $this->session->set_userdata('LaboratoristLoginSession', $session_data);
-                        redirect(base_url('welcome/doc_dash'));
-                    } else {
-                        $this->session->set_flashdata('error', 'Email or Password is Wrong');
-                        redirect(base_url('welcome/login'));
-                    }
-                }
-                //------------------------------------nurse-login-----------------------------//
-
-                elseif ($this->input->post('login_type') == 'nurse') {
-                    $status = $this->user_model->checkPassword4($password, $email);
-                    if ($status != false) {
-
-                        $email = $status->email;
-                        $session_data = array(
-                            'email' => $email,
-                        );
-
-                        $this->session->set_userdata('NurseLoginSession', $session_data);
-                        redirect(base_url('welcome/doc_dash'));
-                    } else {
-                        $this->session->set_flashdata('error', 'Email or Password is Wrong');
-                        redirect(base_url('welcome/login'));
-                    }
-                }
-                //------------------------------------patient-login-----------------------------//
-
-                elseif ($this->input->post('login_type') == 'patient') {
-                    $status = $this->user_model->checkPassword6($password, $email);
-                    if ($status != false) {
-                        // $username = $status->username;
-                        $email = $status->email;
-
-                        $session_data = array(
-                            // 'username'=>$username,
-                            'email' => $email,
-                        );
-
-                        $this->session->set_userdata('PatientLoginSession', $session_data);
-                        redirect(base_url('welcome/doc_dash'));
-                    } else {
-                        $this->session->set_flashdata('error', 'Email or Password is Wrong');
-                        redirect(base_url('welcome/login'));
-                    }
-                }
-                //------------------------------------pharmacist-login-----------------------------//
-
-                elseif ($this->input->post('login_type') == 'pharmacist') {
-                    $status = $this->user_model->checkPassword7($password, $email);
-                    if ($status != false) {
-                        // $username = $status->username;
-                        $email = $status->email;
-
-                        $session_data = array(
-                            // 'username'=>$username,
-                            'email' => $email,
-                        );
-
-                        $this->session->set_userdata('PharmacistLoginSession', $session_data);
-                        redirect(base_url('welcome/doc_dash'));
-                    } else {
-                        $this->session->set_flashdata('error', 'Email or Password is Wrong');
-                        redirect(base_url('welcome/login'));
-                    }
-                }
-                //------------------------------------accountant-login-----------------------------//
-
-                elseif ($this->input->post('login_type') == 'accountant') {
-                    $status = $this->user_model->checkPassword8($password, $email);
-                    if ($status != false) {
-                        // $username = $status->username;
-                        $email = $status->email;
-
-                        $session_data = array(
-                            // 'username'=>$username,
-                            'email' => $email,
-                        );
-
-                        $this->session->set_userdata('AccountantLoginSession', $session_data);
-                        redirect(base_url('welcome/doc_dash'));
-                    } else {
-                        $this->session->set_flashdata('error', 'Email or Password is Wrong');
-                        redirect(base_url('welcome/login'));
-                    }
-                } else {
-                    $this->session->set_flashdata('error', 'Email or Password is Wrong');
-                    redirect(base_url('welcome/login'));
-                }
+                $this->response([
+                    'status' => TRUE,
+                    'message' => 'User login successful.',
+                    'data' => $user
+                ], REST_Controller::HTTP_OK);
             } else {
-                $this->session->set_flashdata('error', 'Fill all the required fields');
-                redirect(base_url('welcome/login'));
+                // Set the response and exit
+                //BAD_REQUEST (400) being the HTTP response code
+                $this->response([
+                    "status" => false,
+                    "error" => "Wrong email or password."
+                ], REST_Controller::HTTP_BAD_REQUEST);
             }
+        } else {
+            // Set the response and exit
+            $this->response([
+                "status" => false,
+                "message" => "Provide email and password.",
+            ], REST_Controller::HTTP_BAD_REQUEST);
         }
     }
 }
