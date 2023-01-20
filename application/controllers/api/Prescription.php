384d0ba7bb1d7f409184d56b7a11584ecc58ba17
insal1998 <insalansari80@gmail.com>
2023-01-20 11:53:57 +0530

    added Prescription 20-01-23

--- application/controllers/api/Prescription.php
+++ application/controllers/api/Prescription.php
@@ -12,6 +12,7 @@ class Prescription extends REST_Controller
         parent::__construct();
         $this->load->database();
         $this->pres = 'prescription';
+        $this->taper = 'taper';
     }
 
     public function prescription_post()
@@ -19,7 +20,7 @@ class Prescription extends REST_Controller
         $pat_id = $this->security->xss_clean($this->input->post('pat_id'));
         $C_id = $this->security->xss_clean($this->input->post('C_id'));
         $staff_id = $this->security->xss_clean($this->input->post('staff_id'));
-        $name = $this->security->xss_clean($this->input->post('name'));
+        $name = $this->security->xss_clean($this->input->post('medicine_name'));
         $type = $this->security->xss_clean($this->input->post('type'));
         $quantity = $this->security->xss_clean($this->input->post('quantity'));
         $frequency = $this->security->xss_clean($this->input->post('frequency'));
@@ -49,7 +50,7 @@ class Prescription extends REST_Controller
                 "pat_id" => $pat_id ?? '',
                 "C_id" => $C_id ?? '',
                 "staff_id" => $staff_id ?? '',
-                "name" => $name ?? '',
+                "medicine_name" => $name ?? '',
                 "type" => $type ?? '',
                 "quantity" => $quantity ?? '',
                 "frequency" => $frequency ?? '',
@@ -85,14 +86,40 @@ class Prescription extends REST_Controller
 
         $data = array();
         if (!empty($C_id)) {
-
-            $data['Prescription'] = $this->db->select("*")->from($this->pres)->where("C_id = '$C_id' XOR pat_id = '$C_id'")->get()->result() ?? [];
+            $data['Prescription'] = $this->db->select('*')->from($this->pres)->where("C_id", $C_id)->get()->result_array();
+            $prescription = $this->db->select('id')->from($this->pres)->where("C_id", $C_id)->get()->result_array();
+            $length = count($prescription);
+            for ($i = 0; $i < $length; ++$i) {
+                $p_id = $prescription[$i]['id'];
+                $data['Prescription'][$i]['Taper'] = $this->db->select("*")->from($this->taper)->where("prescription_id", $p_id)->get()->result_array();
+            }
+            // $C_id = $this->db->select('C_id')->from($this->pres)->where('C_id', $C_id)->get()->result();
         } else {
-            $this->response([
-                'status' => True,
-                'message' => 'Prescription Not Found',
-            ], REST_Controller::HTTP_NOT_FOUND);
+            $master = $this->db->select('DISTINCT(C_id)')->from($this->pres)->get()->result();
+            $length = count($master);
+
+
+            for ($i = 0; $i < $length; ++$i) {
+
+                $C_id = $master[$i]->C_id;
+
+                $data[$i]['Prescription'] = $this->db->select('*')->from($this->pres)->where("C_id", $C_id)->get()->result_array();
+                $prescription = $this->db->select('id')->from($this->pres)->where("C_id", $C_id)->get()->result_array();
+                $length = count($prescription);
+                for ($j = 0; $j < $length; ++$j) {
+                    $p_id = $prescription[$j]['id'];
+                    $data[$i]['Prescription'][$j]['Taper'] = $this->db->select("*")->from($this->taper)->where("prescription_id", $p_id)->get()->result_array();
+                }
+                // print_r($data);
+                // die();
+                // }
+            }
         }
+
+        // $data['Prescription'] = $this->db->select("*")->from($this->pres)->where("C_id = '$C_id' XOR pat_id = '$C_id'")->get()->result() ?? [];
+
+        // $data['Prescription']['Taper'] = $this->db->select("*")->from($this->taper)->where("C_id ", $C_id)->get()->result();
+
 
         if (!empty($data)) {
             $this->response([
@@ -125,6 +152,100 @@ class Prescription extends REST_Controller
                 "status" => FALSE,
                 "message" => "Unable to Delete"
             ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function taper_post()
+    {
+        $medicine_name = $this->security->xss_clean($this->input->post('medicine_name'));
+        $no_of_days = $this->security->xss_clean($this->input->post('no_of_days'));
+        $start_date = $this->security->xss_clean($this->input->post('start_date'));
+        $start_time = $this->security->xss_clean($this->input->post('start_time'));
+        $end_time = $this->security->xss_clean($this->input->post('end_start'));
+        $frequency = $this->security->xss_clean($this->input->post('frequency'));
+        $frequency_unit = $this->security->xss_clean($this->input->post('frequency_unit'));
+        $interval = $this->security->xss_clean($this->input->post('interval'));
+        $interval_unit = $this->security->xss_clean($this->input->post('interval_unit'));
+
+        $C_id = $this->security->xss_clean($this->input->post('C_id'));
+
+        $this->form_validation->set_rules('C_id', 'Cases Id', 'required', array(
+            'required' => 'Case Id is Missing'
+        ));
+
+        $this->form_validation->set_rules('medicine_name', 'Medicine Name', 'required', array(
+            'required' => 'Medicine Name is required'
+        ));
+
+        if ($this->form_validation->run() == false) {
+            $error = strip_tags(validation_errors());
+            $this->response([
+                "status" => False,
+                "mesage" => $error
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        } else {
+            $data = array(
+                "C_id" => $C_id,
+
+                "medicine_name" => $medicine_name,
+                "no_of_days" => $no_of_days,
+                "start_date" => $start_date,
+                "start_time" => $start_time,
+                "end_time" => $end_time,
+                "frequency" => $frequency,
+                "frequency_unit" => $frequency_unit,
+                "interval" => $interval,
+                "interval_unit" => $interval_unit,
+
+                "created_at" => date('Y-m-d H:i:s'),
+            );
+
+            if (!empty($data)) {
+                $insert = $this->db->insert($this->taper, $data);
+
+                if ($insert == True) {
+                    $this->response([
+                        "status" => TRUE,
+                        "message" => "Taper Added Successfully",
+                        "data" => $data,
+                    ], REST_CONTROLLER::HTTP_OK);
+                } else {
+                    $this->response([
+                        "status" => False,
+                        "message" => "Some Error Occured",
+                    ], REST_Controller::HTTP_BAD_REQUEST);
+                }
+            }
+        }
+    }
+
+
+
+    public function taper_get()
+    {
+        $C_id = $this->input->get('C_id');
+
+        $data = array();
+        if (!empty($C_id)) {
+
+            $data['Prescription'] = $this->db->select("*")->from($this->pres)->where("C_id = '$C_id' XOR pat_id = '$C_id'")->get()->result() ?? [];
+        } else {
+            $this->response([
+                'status' => True,
+                'message' => 'Prescription Not Found',
+            ], REST_Controller::HTTP_NOT_FOUND);
+        }
+
+        if (!empty($data)) {
+            $this->response([
+                'status' => true,
+                'data' => $data
+            ], REST_Controller::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'data' => 'Data Not Found.'
+            ], REST_Controller::HTTP_NOT_FOUND);
         }
     }
 }
