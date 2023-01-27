3b22574fc3b57a00e6ae1e4b9c28786d298aefff
insal1998 <insalansari80@gmail.com>
2023-01-27 11:24:26 +0530

    Update in Prescription API 27-01-23

--- application/controllers/api/Prescription.php
+++ application/controllers/api/Prescription.php
@@ -18,7 +18,7 @@ class Prescription extends REST_Controller
     public function prescription_post()
     {
         $pat_id = $this->security->xss_clean($this->input->post('pat_id'));
-        
+
         $C_id = $this->security->xss_clean($this->input->post('C_id'));
         $staff_id = $this->security->xss_clean($this->input->post('staff_id'));
         $name = $this->security->xss_clean($this->input->post('medicine_name'));
@@ -28,7 +28,8 @@ class Prescription extends REST_Controller
         $duration = $this->security->xss_clean($this->input->post('duration'));
         $duration_unit = $this->security->xss_clean($this->input->post('duration_unit'));
         $taper_id = $this->security->xss_clean($this->input->post('taper_id'));
-        $instruction_id = $this->security->xss_clean($this->input->post('instruction_id'));
+        $instruction = $this->security->xss_clean($this->input->post('instruction'));
+        // $instruction_id = $this->security->xss_clean($this->input->post('instruction_id'));
 
         $this->form_validation->set_rules('C_id', 'Cases ID', 'required', array(
             'required' => 'Case ID is Missing'
@@ -39,6 +40,9 @@ class Prescription extends REST_Controller
         $this->form_validation->set_rules('staff_id', 'Doctor ID', 'required', array(
             'required' => 'Doctor ID is Missing'
         ));
+        $this->form_validation->set_rules('instruction', 'Instruction', 'required', array(
+            'required' => 'Please Provide Instructions for Medication'
+        ));
 
         if ($this->form_validation->run() == false) {
             $error = strip_tags(validation_errors());
@@ -48,9 +52,9 @@ class Prescription extends REST_Controller
             ], REST_Controller::HTTP_BAD_REQUEST);
         } else {
             $data = array(
-                "pat_id" => $pat_id ?? '',
-                "C_id" => $C_id ?? '',
-                "staff_id" => $staff_id ?? '',
+                "pat_id" => $pat_id,
+                "C_id" => $C_id,
+                "staff_id" => $staff_id,
                 "medicine_name" => $name ?? '',
                 "type" => $type ?? '',
                 "quantity" => $quantity ?? '',
@@ -58,7 +62,8 @@ class Prescription extends REST_Controller
                 "duration" => $duration ?? '',
                 "duration_unit" => $duration_unit ?? '',
                 "taper_id" => $taper_id ?? '',
-                "instruction_id" => $instruction_id ?? '',
+                "instruction" => $instruction,
+                // // "instruction_id" => $instruction_id ?? '',
 
                 "created_at" => date('Y-m-d H:i:s'),
             );
@@ -159,204 +164,204 @@ class Prescription extends REST_Controller
         }
     }
 
-    public function taper_post()
-    {
-        $medicine_name = $this->security->xss_clean($this->input->post('medicine_name'));
-        $no_of_days = $this->security->xss_clean($this->input->post('no_of_days'));
-        $start_date = $this->security->xss_clean($this->input->post('start_date'));
-        $start_time = $this->security->xss_clean($this->input->post('start_time'));
-        $end_time = $this->security->xss_clean($this->input->post('end_start'));
-        $frequency = $this->security->xss_clean($this->input->post('frequency'));
-        $frequency_unit = $this->security->xss_clean($this->input->post('frequency_unit'));
-        $interval = $this->security->xss_clean($this->input->post('interval'));
-        $interval_unit = $this->security->xss_clean($this->input->post('interval_unit'));
+    // public function taper_post()
+    // {
+    //     $medicine_name = $this->security->xss_clean($this->input->post('medicine_name'));
+    //     $no_of_days = $this->security->xss_clean($this->input->post('no_of_days'));
+    //     $start_date = $this->security->xss_clean($this->input->post('start_date'));
+    //     $start_time = $this->security->xss_clean($this->input->post('start_time'));
+    //     $end_time = $this->security->xss_clean($this->input->post('end_start'));
+    //     $frequency = $this->security->xss_clean($this->input->post('frequency'));
+    //     $frequency_unit = $this->security->xss_clean($this->input->post('frequency_unit'));
+    //     $interval = $this->security->xss_clean($this->input->post('interval'));
+    //     $interval_unit = $this->security->xss_clean($this->input->post('interval_unit'));
 
-        $prescription_id = $this->security->xss_clean($this->input->post('prescription_id'));
+    //     $prescription_id = $this->security->xss_clean($this->input->post('prescription_id'));
 
-        $this->form_validation->set_rules('prescription_id', 'Prescription ID', 'required', array(
-            'required' => 'Prescription ID is Missing'
-        ));
+    //     $this->form_validation->set_rules('prescription_id', 'Prescription ID', 'required', array(
+    //         'required' => 'Prescription ID is Missing'
+    //     ));
 
-        $this->form_validation->set_rules('medicine_name', 'Medicine Name', 'required', array(
-            'required' => 'Medicine Name is required'
-        ));
+    //     $this->form_validation->set_rules('medicine_name', 'Medicine Name', 'required', array(
+    //         'required' => 'Medicine Name is required'
+    //     ));
 
-        if ($this->form_validation->run() == false) {
-            $error = strip_tags(validation_errors());
-            $this->response([
-                "status" => False,
-                "mesage" => $error
-            ], REST_Controller::HTTP_BAD_REQUEST);
-        } else {
-            $data = array(
-                "prescription_id" => $prescription_id,
+    //     if ($this->form_validation->run() == false) {
+    //         $error = strip_tags(validation_errors());
+    //         $this->response([
+    //             "status" => False,
+    //             "mesage" => $error
+    //         ], REST_Controller::HTTP_BAD_REQUEST);
+    //     } else {
+    //         $data = array(
+    //             "prescription_id" => $prescription_id,
 
-                "medicine_name" => $medicine_name,
-                "no_of_days" => $no_of_days,
-                "start_date" => $start_date,
-                "start_time" => $start_time,
-                "end_time" => $end_time,
-                "frequency" => $frequency,
-                "frequency_unit" => $frequency_unit,
-                "interval" => $interval,
-                "interval_unit" => $interval_unit,
+    //             "medicine_name" => $medicine_name,
+    //             "no_of_days" => $no_of_days,
+    //             "start_date" => $start_date,
+    //             "start_time" => $start_time,
+    //             "end_time" => $end_time,
+    //             "frequency" => $frequency,
+    //             "frequency_unit" => $frequency_unit,
+    //             "interval" => $interval,
+    //             "interval_unit" => $interval_unit,
 
-                "created_at" => date('Y-m-d H:i:s'),
-            );
+    //             "created_at" => date('Y-m-d H:i:s'),
+    //         );
 
-            if (!empty($data)) {
-                $insert = $this->db->insert($this->taper, $data);
+    //         if (!empty($data)) {
+    //             $insert = $this->db->insert($this->taper, $data);
 
-                if ($insert == True) {
-                    $this->response([
-                        "status" => TRUE,
-                        "message" => "Taper Added Successfully",
-                        "data" => $data,
-                    ], REST_CONTROLLER::HTTP_OK);
-                } else {
-                    $this->response([
-                        "status" => False,
-                        "message" => "Unable to add Taper",
-                    ], REST_Controller::HTTP_BAD_REQUEST);
-                }
-            }
-        }
-    }
+    //             if ($insert == True) {
+    //                 $this->response([
+    //                     "status" => TRUE,
+    //                     "message" => "Taper Added Successfully",
+    //                     "data" => $data,
+    //                 ], REST_CONTROLLER::HTTP_OK);
+    //             } else {
+    //                 $this->response([
+    //                     "status" => False,
+    //                     "message" => "Unable to add Taper",
+    //                 ], REST_Controller::HTTP_BAD_REQUEST);
+    //             }
+    //         }
+    //     }
+    // }
 
-    public function taper_update_post()
-    {
-        $id = $this->security->xss_clean($this->input->post('id'));
+    // public function taper_update_post()
+    // {
+    //     $id = $this->security->xss_clean($this->input->post('id'));
 
-        $medicine_name = $this->security->xss_clean($this->input->post('medicine_name'));
-        $no_of_days = $this->security->xss_clean($this->input->post('no_of_days'));
-        $start_date = $this->security->xss_clean($this->input->post('start_date'));
-        $start_time = $this->security->xss_clean($this->input->post('start_time'));
-        $end_time = $this->security->xss_clean($this->input->post('end_start'));
-        $frequency = $this->security->xss_clean($this->input->post('frequency'));
-        $frequency_unit = $this->security->xss_clean($this->input->post('frequency_unit'));
-        $interval = $this->security->xss_clean($this->input->post('interval'));
-        $interval_unit = $this->security->xss_clean($this->input->post('interval_unit'));
+    //     $medicine_name = $this->security->xss_clean($this->input->post('medicine_name'));
+    //     $no_of_days = $this->security->xss_clean($this->input->post('no_of_days'));
+    //     $start_date = $this->security->xss_clean($this->input->post('start_date'));
+    //     $start_time = $this->security->xss_clean($this->input->post('start_time'));
+    //     $end_time = $this->security->xss_clean($this->input->post('end_start'));
+    //     $frequency = $this->security->xss_clean($this->input->post('frequency'));
+    //     $frequency_unit = $this->security->xss_clean($this->input->post('frequency_unit'));
+    //     $interval = $this->security->xss_clean($this->input->post('interval'));
+    //     $interval_unit = $this->security->xss_clean($this->input->post('interval_unit'));
 
-        $prescription_id = $this->security->xss_clean($this->input->post('prescription_id'));
+    //     $prescription_id = $this->security->xss_clean($this->input->post('prescription_id'));
 
-        $data = array();
-        if (!empty($medicine_name) && !empty($id)) {
-            $data['medicine_name'] = $medicine_name;
-        }
-        if (!empty($no_of_days) && !empty($id)) {
-            $data['no_of_days'] = $no_of_days;
-        }
-        if (!empty($start_date) && !empty($id)) {
-            $data['start_date'] = $start_date;
-        }
-        if (!empty($start_time) && !empty($id)) {
-            $data['start_time'] = $start_time;
-        }
-        if (!empty($end_time) && !empty($id)) {
-            $data['end_time'] = $end_time;
-        }
-        if (!empty($frequency) && !empty($id)) {
-            $data['frequency'] = $frequency;
-        }
-        if (!empty($frequency_unit) && !empty($id)) {
-            $data['frequency_unit'] = $frequency_unit;
-        }
-        if (!empty($interval) && !empty($id)) {
-            $data['interval'] = $interval;
-        }
-        if (!empty($interval_unit) && !empty($id)) {
-            $data['interval_unit'] = $interval_unit;
-        }
-        if (!empty($prescription_id) && !empty($id)) {
-            $data['prescription_id'] = $prescription_id;
-        }
+    //     $data = array();
+    //     if (!empty($medicine_name) && !empty($id)) {
+    //         $data['medicine_name'] = $medicine_name;
+    //     }
+    //     if (!empty($no_of_days) && !empty($id)) {
+    //         $data['no_of_days'] = $no_of_days;
+    //     }
+    //     if (!empty($start_date) && !empty($id)) {
+    //         $data['start_date'] = $start_date;
+    //     }
+    //     if (!empty($start_time) && !empty($id)) {
+    //         $data['start_time'] = $start_time;
+    //     }
+    //     if (!empty($end_time) && !empty($id)) {
+    //         $data['end_time'] = $end_time;
+    //     }
+    //     if (!empty($frequency) && !empty($id)) {
+    //         $data['frequency'] = $frequency;
+    //     }
+    //     if (!empty($frequency_unit) && !empty($id)) {
+    //         $data['frequency_unit'] = $frequency_unit;
+    //     }
+    //     if (!empty($interval) && !empty($id)) {
+    //         $data['interval'] = $interval;
+    //     }
+    //     if (!empty($interval_unit) && !empty($id)) {
+    //         $data['interval_unit'] = $interval_unit;
+    //     }
+    //     if (!empty($prescription_id) && !empty($id)) {
+    //         $data['prescription_id'] = $prescription_id;
+    //     }
 
-        $update = $this->db->update($this->taper, $data, array('id' => $id));
+    //     $update = $this->db->update($this->taper, $data, array('id' => $id));
 
-        if ($update) {
-            $this->response([
-                "status" => true,
-                "message" => "Data Updated Successfully"
-            ], REST_Controller::HTTP_OK);
-        } else {
-            $this->response([
-                "status" => false,
-                "message" => "Data Update Unsuccessful"
-            ], REST_Controller::HTTP_BAD_REQUEST);
-        }
-    }
+    //     if ($update) {
+    //         $this->response([
+    //             "status" => true,
+    //             "message" => "Data Updated Successfully"
+    //         ], REST_Controller::HTTP_OK);
+    //     } else {
+    //         $this->response([
+    //             "status" => false,
+    //             "message" => "Data Update Unsuccessful"
+    //         ], REST_Controller::HTTP_BAD_REQUEST);
+    //     }
+    // }
 
-    public function instruction_post()
-    {
-        $instruction = $this->security->xss_clean($this->input->post('instruction'));
+    // public function instruction_post()
+    // {
+    //     $instruction = $this->security->xss_clean($this->input->post('instruction'));
 
-        $prescription_id = $this->security->xss_clean($this->input->post('prescription_id'));
+    //     $prescription_id = $this->security->xss_clean($this->input->post('prescription_id'));
 
-        $this->form_validation->set_rules('prescription_id', 'Prescription ID', 'required', array(
-            'required' => 'Prescription ID is Missing'
-        ));
+    //     $this->form_validation->set_rules('prescription_id', 'Prescription ID', 'required', array(
+    //         'required' => 'Prescription ID is Missing'
+    //     ));
 
-        if ($this->form_validation->run() == false) {
-            $error = strip_tags(validation_errors());
-            $this->response([
-                "status" => False,
-                "mesage" => $error
-            ], REST_Controller::HTTP_BAD_REQUEST);
-        } else {
-            $data = array(
-                "prescription_id" => $prescription_id,
-                "instruction" => $instruction,
+    //     if ($this->form_validation->run() == false) {
+    //         $error = strip_tags(validation_errors());
+    //         $this->response([
+    //             "status" => False,
+    //             "mesage" => $error
+    //         ], REST_Controller::HTTP_BAD_REQUEST);
+    //     } else {
+    //         $data = array(
+    //             "prescription_id" => $prescription_id,
+    //             "instruction" => $instruction,
 
-                "created_at" => date('Y-m-d H:i:s'),
-            );
+    //             "created_at" => date('Y-m-d H:i:s'),
+    //         );
 
-            if (!empty($data)) {
-                $insert = $this->db->insert($this->pro, $data);
+    //         if (!empty($data)) {
+    //             $insert = $this->db->insert($this->pro, $data);
 
-                if ($insert == True) {
-                    $this->response([
-                        "status" => TRUE,
-                        "message" => "Instruction Added Successfully",
-                        "data" => $data,
-                    ], REST_CONTROLLER::HTTP_OK);
-                } else {
-                    $this->response([
-                        "status" => False,
-                        "message" => "Unable to add Instructions",
-                    ], REST_Controller::HTTP_BAD_REQUEST);
-                }
-            }
-        }
-    }
+    //             if ($insert == True) {
+    //                 $this->response([
+    //                     "status" => TRUE,
+    //                     "message" => "Instruction Added Successfully",
+    //                     "data" => $data,
+    //                 ], REST_CONTROLLER::HTTP_OK);
+    //             } else {
+    //                 $this->response([
+    //                     "status" => False,
+    //                     "message" => "Unable to add Instructions",
+    //                 ], REST_Controller::HTTP_BAD_REQUEST);
+    //             }
+    //         }
+    //     }
+    // }
 
-    public function instruction_update_post()
-    {
-        $id = $this->security->xss_clean($this->input->post('id'));
+    // public function instruction_update_post()
+    // {
+    //     $id = $this->security->xss_clean($this->input->post('id'));
 
-        $instruction = $this->security->xss_clean($this->input->post('instruction'));
+    //     $instruction = $this->security->xss_clean($this->input->post('instruction'));
 
-        $prescription_id = $this->security->xss_clean($this->input->post('prescription_id'));
+    //     $prescription_id = $this->security->xss_clean($this->input->post('prescription_id'));
 
-        $data = array();
-        if (!empty($instruction) && !empty($id)) {
-            $data['instruction'] = $instruction;
-        }
-        if (!empty($prescription_id) && !empty($id)) {
-            $data['prescription_id'] = $prescription_id;
-        }
+    //     $data = array();
+    //     if (!empty($instruction) && !empty($id)) {
+    //         $data['instruction'] = $instruction;
+    //     }
+    //     if (!empty($prescription_id) && !empty($id)) {
+    //         $data['prescription_id'] = $prescription_id;
+    //     }
 
-        $update = $this->db->update($this->pro, $data, array('id' => $id));
+    //     $update = $this->db->update($this->pro, $data, array('id' => $id));
 
-        if ($update) {
-            $this->response([
-                "status" => true,
-                "message" => "Data Updated Successfully"
-            ], REST_Controller::HTTP_OK);
-        } else {
-            $this->response([
-                "status" => false,
-                "message" => "Data Update Unsuccessful"
-            ], REST_Controller::HTTP_BAD_REQUEST);
-        }
-    }
+    //     if ($update) {
+    //         $this->response([
+    //             "status" => true,
+    //             "message" => "Data Updated Successfully"
+    //         ], REST_Controller::HTTP_OK);
+    //     } else {
+    //         $this->response([
+    //             "status" => false,
+    //             "message" => "Data Update Unsuccessful"
+    //         ], REST_Controller::HTTP_BAD_REQUEST);
+    //     }
+    // }
 }
